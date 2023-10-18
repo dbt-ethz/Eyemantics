7c4ad9af35cde617a2bd654da4169b41806542d3
wenqian157 <yang@arch.ethz.ch>
2023-10-18 16:30:33 +0200

    update image capturing

--- EyemanticsUnity/Assets/Scripts/CameraRendering.cs
+++ EyemanticsUnity/Assets/Scripts/CameraRendering.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.MagicLeap;
 
 public class CameraRendering : MonoBehaviour
 {
-    [SerializeField, Tooltip("The renderer to show the camera capture on RGB format")]
-    private Renderer _screenRendererRGB = null;
-    private Texture2D videoTextureRGB;
+    //[SerializeField, Tooltip("The UI to show the camera capture in JPEG format")]
+    //private RawImage _screenRendererJPEG = null;
+    private Texture2D _imageTexture;
     private MLCamera _camera;
     [SerializeField, Tooltip("Desired width for the camera capture")]
     private int captureWidth = 1280;
@@ -26,7 +27,7 @@ public class CameraRendering : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            SaveTexture(videoTextureRGB, captureWidth, captureHeight);
+            ImageCapture();
         }
     }
 
@@ -44,7 +45,7 @@ public class CameraRendering : MonoBehaviour
     private void ConfigureCameraInput()
     {
         //Gets the stream capabilities the selected camera. (Supported capture types, formats and resolutions)
-        MLCamera.StreamCapability[] streamCapabilities = MLCamera.GetImageStreamCapabilitiesForCamera(_camera, MLCamera.CaptureType.Video);
+        MLCamera.StreamCapability[] streamCapabilities = MLCamera.GetImageStreamCapabilitiesForCamera(_camera, MLCamera.CaptureType.Image);
 
         if (streamCapabilities.Length == 0)
             return;
@@ -54,7 +55,7 @@ public class CameraRendering : MonoBehaviour
 
         //Try to get the stream that most closely matches the target width and height
         if (MLCamera.TryGetBestFitStreamCapabilityFromCollection(streamCapabilities, captureWidth, captureHeight,
-                MLCamera.CaptureType.Video, out MLCamera.StreamCapability selectedCapability))
+                MLCamera.CaptureType.Image, out MLCamera.StreamCapability selectedCapability))
         {
             defaultCapability = selectedCapability;
         }
@@ -62,76 +63,68 @@ public class CameraRendering : MonoBehaviour
         //Initialize a new capture config.
         _captureConfig = new MLCamera.CaptureConfig();
         //Set RGBA video as the output
-        MLCamera.OutputFormat outputFormat = MLCamera.OutputFormat.RGBA_8888;
-        //Set the Frame Rate to 30fps
-        _captureConfig.CaptureFrameRate = MLCamera.CaptureFrameRate._30FPS;
+        MLCamera.OutputFormat outputFormat = MLCamera.OutputFormat.JPEG;
+        //Set the Frame Rate as none for image capturing
+        _captureConfig.CaptureFrameRate = MLCamera.CaptureFrameRate.None;
         //Initialize a camera stream config.
         //The Main Camera can support up to two stream configurations
         _captureConfig.StreamConfigs = new MLCamera.CaptureStreamConfig[1];
         _captureConfig.StreamConfigs[0] = MLCamera.CaptureStreamConfig.Create(
             defaultCapability, outputFormat
         );
-        //StartVideoCapture();
+    }
+    private void ImageCapture()
+    {
+        MLResult result = _camera.PrepareCapture(_captureConfig, out MLCamera.Metadata metaData);
+        if (result.IsOk)
+        {
+            // Trigger auto exposure and auto white balance
+            _camera.PreCaptureAEAWB();
+            result = _camera.CaptureImage();
+            if (result.IsOk)
+            {
+                Debug.Log("capture image!");
+            }
+            else
+            {
+                Debug.LogError("Failed to start image capture!");
+            }
+        }
     }
     private void SetCameraCallbacks()
     {
-        //Provides frames in either YUV/RGBA format depending on the stream configuration
-        _camera.OnRawVideoFrameAvailable += RawVideoFrameAvailable;
+        _camera.OnRawImageAvailable += RowImageAvailable;
+        Debug.Log("set camera call backs");
     }
-    void RawVideoFrameAvailable(MLCamera.CameraOutput output, MLCamera.ResultExtras extras, MLCamera.Metadata metadataHandle)
+    void RowImageAvailable(MLCamera.CameraOutput output, MLCamera.ResultExtras extras, MLCamera.Metadata metadataHandle)
     {
-        if (output.Format == MLCamera.OutputFormat.RGBA_8888)
+        if (output.Format == MLCamera.OutputFormat.JPEG)
         {
-            UpdateRGBTexture(ref videoTextureRGB, output.Planes[0], _screenRendererRGB);
+            UpdateJPGTexture(output.Planes[0]);
         }
-    }
-
-    private void UpdateRGBTexture(ref Texture2D videoTextureRGB, MLCamera.PlaneInfo imagePlane, Renderer renderer)
-    {
-
-        if (videoTextureRGB != null &&
-            (videoTextureRGB.width != imagePlane.Width || videoTextureRGB.height != imagePlane.Height))
-        {
-            Destroy(videoTextureRGB);
-            videoTextureRGB = null;
-        }
-
-        if (videoTextureRGB == null)
-        {
-            videoTextureRGB = new Texture2D((int)imagePlane.Width, (int)imagePlane.Height, TextureFormat.RGBA32, false);
-            videoTextureRGB.filterMode = FilterMode.Bilinear;
-
-            Material material = renderer.material;
-            material.mainTexture = videoTextureRGB;
-            material.mainTextureScale = new Vector2(1.0f, -1.0f);
-        }
-
-        int actualWidth = (int)(imagePlane.Width * imagePlane.PixelStride);
-
-        if (imagePlane.Stride != actualWidth)
-        {
-            var newTextureChannel = new byte[actualWidth * imagePlane.Height];
-            for (int i = 0; i < imagePlane.Height; i++)
-            {
-                Buffer.BlockCopy(imagePlane.Data, (int)(i * imagePlane.Stride), newTextureChannel, i * actualWidth, actualWidth);
-            }
-            videoTextureRGB.LoadRawTextureData(newTextureChannel);
-        }
-        else
-        {
-            videoTextureRGB.LoadRawTextureData(imagePlane.Data);
-        }
-
-        videoTextureRGB.Apply();
     }
     private void SaveTexture(Texture2D image, int resWidth, int resHeight)
     {
-        image.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
-        //image.Apply();
-
         byte[] bytes = image.EncodeToPNG();
         Destroy(image);
 
         File.WriteAllBytes(Application.dataPath + "/Images/" + Time.time + ".png", bytes);
+    }
+    private void UpdateJPGTexture(MLCamera.PlaneInfo imagePlane)
+    {
+        if (_imageTexture != null)
+        {
+            Destroy(_imageTexture);
+        }
+
+        _imageTexture = new Texture2D(8, 8);
+        bool status = _imageTexture.LoadImage(imagePlane.Data);
+        if (status && (_imageTexture.width != 8 && _imageTexture.height != 8))
+        {
+            //_screenRendererJPEG.texture = _imageTexture;
+            SaveTexture(_imageTexture, captureWidth, captureHeight);
+        }
+        
+        
     }
 }
